9cf3e948374a4939303b113f6bff7e9e83a98c4b
LipinVV <vit.lipin@gmail.com>
2024-07-31 23:26:27 +0300

    MongoDB - part 3

--- src/routes/book.js
+++ src/routes/book.js
@@ -2,8 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 const { errorMessage } = require('../../constants').dictionary;
-const { books } = require('../../books-store');
-const Book = require("../Book/Book");
+const Book = require('../../models/Book');
 
 const MAIN = '/';
 const ID = '/:id';
@@ -13,8 +12,13 @@ const BOOKS = '/books';
 const COUNTER_API = process.env.COUNTER_API || 'http://counter:3003/counter';
 
 // Get all books
-router.get(MAIN, (req, res) => {
-    res.render('books', { books: books, currentRoute: BOOKS, title: 'All Books' });
+router.get(MAIN, async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.render('books', { books: books, currentRoute: BOOKS, title: 'All Books' });
+    } catch (error) {
+        res.status(500).json(errorMessage);
+    }
 });
 
 // Render create book page
@@ -25,61 +29,85 @@ router.get(CREATE, (req, res) => {
         authors: '',
         favourite: false,
     };
-    res.render('create', { currentRoute: CREATE, book: book });
+    res.render('create', { currentRoute: CREATE, book: book, title: 'New book' });
 });
 
 // Create a new book
-router.post(CREATE, (req, res) => {
+router.post(CREATE, async (req, res) => {
     const { title, description, authors, favourite } = req.body;
-    const newId = (books.length + 1).toString();
-    const newBook = new Book(title, description, authors, favourite, '', '', '', newId);
-    books.push(newBook);
-    res.redirect(BOOKS);
+    try {
+        const bookCount = await Book.countDocuments(); // для подсчета количества документов в коллекции, соответствующих определенным критериям
+        const newBook = new Book({
+            title,
+            description,
+            authors,
+            favourite: favourite === 'on',
+            id: (bookCount + 1).toString(),
+        });
+        await newBook.save(); // нативный метод, который выполняет операцию записи в MongoDB
+        res.redirect(BOOKS);
+    } catch (error) {
+        res.status(500).json(errorMessage);
+    }
 });
 
 // Get book by id and increment counter
 router.get(ID, async (req, res) => {
     const { id } = req.params;
-    const book = books.find(book => book.id === id);
-    if (book) {
-        try {
-            const request  = await axios.post(`${COUNTER_API}/${id}/incr`);
-            const { data } = request;
-            res.render('book', { book: book, currentRoute: '/book', viewData: data });
-        } catch (error) {
-            res.render('book', { book: book, currentRoute: '/book', viewData: 'N/A' });
+    try {
+        const book = await Book.findOne({ id });
+        if (book) {
+            try {
+                const request = await axios.post(`${COUNTER_API}/${id}/incr`);
+                const { data } = request;
+                res.render('book', { book: book, currentRoute: '/book', viewData: data, title: book.title });
+            } catch (error) {
+                res.render('book', { book: book, currentRoute: '/book', viewData: 'N/A', title: 'Not found' });
+            }
+        } else {
+            res.status(404).json(errorMessage);
         }
-    } else {
-        res.status(404).json(errorMessage);
+    } catch (error) {
+        res.status(500).json(errorMessage);
     }
 });
 
 // Render edit book page
-router.get(ID + EDIT, (req, res) => {
+router.get(ID + EDIT, async (req, res) => {
     const { id } = req.params;
-    const book = books.find(book => book.id === id);
-    if (book) {
-        res.render('update', { book: book, currentRoute: EDIT });
-    } else {
-        res.status(404).json(errorMessage);
+    try {
+        const book = await Book.findOne({ id });
+        if (book) {
+            res.render('update', { book: book, currentRoute: EDIT, title: book.title });
+        } else {
+            res.status(404).json(errorMessage);
+        }
+    } catch (error) {
+        res.status(500).json(errorMessage);
     }
 });
 
 // Update a book by id
-router.post(ID + EDIT, (req, res) => {
+router.post(ID + EDIT, async (req, res) => {
     const { title, description, authors, favourite } = req.body;
     const { id } = req.params;
-    const bookIndex = books.findIndex(book => book.id === id);
     const isFavourite = favourite !== undefined && favourite.toString() === 'on';
 
-    if (bookIndex !== -1) {
-        books[bookIndex] = {
-            ...books[bookIndex],
-            title, description, authors, favourite: isFavourite,
-        };
-        res.redirect(`${BOOKS}/${id}`);
-    } else {
-        res.status(404).json(errorMessage);
+    try {
+        const book = await Book.findOneAndUpdate({ id }, {
+            title,
+            description,
+            authors,
+            favourite: isFavourite
+        }, { new: true }); // без этой опции по умолчанию метод возвращает документ до обновления
+
+        if (book) {
+            res.redirect(`${BOOKS}/${id}`);
+        } else {
+            res.status(404).json(errorMessage);
+        }
+    } catch (error) {
+        res.status(500).json(errorMessage);
     }
 });
 
